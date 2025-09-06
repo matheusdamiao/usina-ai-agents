22500ad2ba58d9569f36cbe361e1a79b187275b7
Matheus Damião <matheus.damiaoliveira@gmail.com>
2025-09-06 18:23:38 -0300

    feat add clinic agent

---
+++ src/mastra/mcp/mcp-client.ts
@@ -0,0 +1,35 @@
+import { MCPClient } from "@mastra/mcp";
+ 
+export const mcp = new MCPClient({
+  servers: {
+    // googleCalendar: {
+    //   url: new URL("https://apollo-rf6v9rs8q-composio.vercel.app/v3/mcp/89cb5940-48e1-4173-acce-ddda488eebf5/mcp?include_composio_helper_actions=true"),
+    // },
+    googleCalendarMcp: {
+       "command": "npx",
+      "args": [
+        "-y",
+        "@smithery/cli@latest",
+        "run",
+        "@goldk3y/google-calendar-mcp",
+        "--key",
+        "643e073f-1dd6-47a9-85f6-79422556ef01",
+        "--profile",
+        "vicious-rhinoceros-JwWsAj"
+      ]
+    }
+    // gsuitemcp: {
+    //   "command": "npx",
+    //   "args": [
+    //     "-y",
+    //     "@smithery/cli@latest",
+    //     "run",
+    //     "@rishipradeep-think41/gsuite-mcp",
+    //     "--key",
+    //     "643e073f-1dd6-47a9-85f6-79422556ef01",
+    //     "--profile",
+    //     "vicious-rhinoceros-JwWsAj"
+    //   ]
+    // }
+  },
+});
