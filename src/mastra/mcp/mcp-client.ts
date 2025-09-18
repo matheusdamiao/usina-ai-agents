193ba200f3178efa48d6bc584cdd5eedf1ff7675
Matheus Damião <matheus.damiaoliveira@gmail.com>
2025-09-17 21:06:36 -0300

    feat: remove mcp server

--- src/mastra/mcp/mcp-client.ts
+++ src/mastra/mcp/mcp-client.ts
@@ -5,19 +5,19 @@ export const mcp = new MCPClient({
     // googleCalendar: {
     //   url: new URL("https://apollo-rf6v9rs8q-composio.vercel.app/v3/mcp/89cb5940-48e1-4173-acce-ddda488eebf5/mcp?include_composio_helper_actions=true"),
     // },
-    googleCalendarMcp: {
-      "command": "npx",
-      "args": [
-        "-y",
-        "@smithery/cli@latest",
-        "run",
-        "@goldk3y/google-calendar-mcp",
-        "--key",
-        "643e073f-1dd6-47a9-85f6-79422556ef01",
-        "--profile",
-        "vicious-rhinoceros-JwWsAj"
-      ]
-    }
+    // googleCalendarMcp: {
+    //   "command": "npx",
+    //   "args": [
+    //     "-y",
+    //     "@smithery/cli@latest",
+    //     "run",
+    //     "@goldk3y/google-calendar-mcp",
+    //     "--key",
+    //     "643e073f-1dd6-47a9-85f6-79422556ef01",
+    //     "--profile",
+    //     "vicious-rhinoceros-JwWsAj"
+    //   ]
+    // }
     // gsuitemcp: {
     //   "command": "npx",
     //   "args": [
